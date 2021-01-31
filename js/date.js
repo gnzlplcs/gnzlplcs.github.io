28ef2c9886a586b877570329b94362575f4d8c86
gnzlplcs <palaciosgm@outlook.com>
2021-01-30 22:40:48 -0500

    Date in main index

--- js/date.js
+++ js/date.js
@@ -1,2 +1,5 @@
-let lastModif = new Date(document.lastModified)  
-document.getElementById("lastUpdate").innerHTML = lastModif;
+const months = ["January", "February", "March","April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let currentDate = new Date()
+let result = weekdays[currentDate.getDay()] + ", " + currentDate.getDate() + " " + months[currentDate.getMonth()] + " "  + currentDate.getFullYear();
+document.getElementById('lastUpdate').innerHTML = result;
