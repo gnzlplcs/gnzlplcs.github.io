64975a2a2466aa6c912b81e24512f6d0167415f7
gnzlplcs <palaciosgm@outlook.com>
2021-02-05 17:33:32 -0500

    Article section: done

---
+++ lesson5/js/banner.js
@@ -0,0 +1,7 @@
+const completeDate = new Date();
+let today = completeDate.getDay();
+let element = document.getElementById('friday-banner');
+if (today == 5) {
+    element.style.display = "block";
+}
+console.log(element);
--- lesson5/js/currentDay.js
+++ lesson5/js/currentDay.js
@@ -1,10 +1,10 @@
 const date = new Date();
 const days = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
-const day1 = days[date.getDay()];
-const day2 = days[date.getDay() + 1];
-const day3 = days[date.getDay() + 2];
-const day4 = days[date.getDay() + 3];
-const day5 = days[date.getDay() + 4];
+let day1 = days[date.getDay()];
+let day2 = days[date.getDay() + 1];
+let day3 = days[date.getDay() + 2];
+let day4 = days[date.getDay() + 3];
+let day5 = days[date.getDay() + 4];
 document.getElementById('day1').innerHTML = day1;
 document.getElementById('day2').innerHTML = day2;
 document.getElementById('day3').innerHTML = day3;
