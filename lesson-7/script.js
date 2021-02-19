0163377025c7d6b38bffa774f4d3a18fe2e1eb09
gnzlplcs <palaciosgm@outlook.com>
2021-02-19 00:04:05 -0500

    lazy loading

---
+++ lesson-7/script.js
@@ -0,0 +1,31 @@
+let imagesToLoad = document.querySelectorAll('img[data-src]');
+const loadImages = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {
+        image.removeAttribute('data-src');
+    }
+}
+
+imagesToLoad.forEach((img) => {
+    loadImages(img);
+})  
+
+// Intersection Observer 
+if('IntersectionObserver' in window){
+    const observer = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
+            if(item.isIntersecting) {
+                loadImages(items.target) ;  
+                observer.unobserve(item.target);
+            }
+        });
+    });
+}
+
+imagesToLoad.forEach((img) => {
+    observer.observe(img);
+}) else {
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
+    });
+}
