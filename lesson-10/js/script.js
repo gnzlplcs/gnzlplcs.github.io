27e642b211948213a4c7ba486a5a5eb35a6b87f8
gnzlplcs <palaciosgm@outlook.com>
2021-03-13 19:27:13 -0500

    I hope it wion't be necessary more settings

--- lesson-10/js/script.js
+++ lesson-10/js/script.js
@@ -1,5 +1,4 @@
 // -------------DATE---------------
-
 let myData = new Date(document.lastModified);
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
@@ -27,43 +26,77 @@ function toggle() {
 }
 
 // ---------------JSON Request-----------------------
-const requestURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=96903c0c4a665e1829d511ed75451893&units=imperial";
+let cityID = 5604473;
+let appid = "96903c0c4a665e1829d511ed75451893";
 
-fetch(requestURL)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (jsonObject) {
-        console.log(jsonObject)
-        document.getElementById('temperature').textContent = jsonObject.main.temp;
-        document.getElementById('max-temp').textContent = jsonObject.main.temp_max;
-        document.getElementById('min-temp').textContent = jsonObject.main.temp_min;
+// ----------Weather Summary-------------
+const requestURLweather = `https://api.openweathermap.org/data/2.5/weather?id=${cityID}&appid=${appid}&units=imperial`;
+
+fetch(requestURLweather)
+    .then((response) => response.json())
+    .then((jsonObject) => {
+        console.log(jsonObject);
+        document.getElementById('temperature').textContent = jsonObject.main.temp.toFixed(0);
+        document.getElementById('max-temp').textContent = jsonObject.main.temp_max.toFixed(0);
+        document.getElementById('min-temp').textContent = jsonObject.main.temp_min.toFixed(0);
         document.getElementById('humidity').textContent = jsonObject.main.humidity;
         document.getElementById('wind').textContent = jsonObject.wind.speed;
 
+        // -------------------Setting up Wind Chill----------------
         let t = parseFloat(jsonObject.main.temp);
         let s = parseFloat(jsonObject.wind.speed);
         let f = calc(t, s);
-
         function calc(x, y) {
             let result = 35.74 + (0.6215 * x) - (35.75 * Math.pow(y, 0.16)) + (0.4275 * x * Math.pow(y, 0.16));
-            return result.toFixed(1);
+            return result.toFixed(0);
+        }
+        let finalResult = (t <= 50 && s > 3) ? `${f}°F` : 'N/A';
+        document.getElementById('windchill').textContent = finalResult;
+    });
+
+// -------------Forecast------------
+const requestURLforecast = `https://api.openweathermap.org/data/2.5/forecast?id=${cityID}&appid=${appid}&units=imperial`;
+
+fetch(requestURLforecast)
+    .then((response) => response.json())
+    .then((jsonObject) => {
+        // ----------------Declaring variables---------------
+        const dayOfWeek = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+        let daysArray = [];
+        let iconsArray = [];
+        let descIconArray = [];
+        let tempArray = [];
+
+        // --------------Setting up arrays----------------------
+        for (let i = 0; i < jsonObject.list.length; i++) {
+            if (jsonObject.list[i].dt_txt.indexOf('18:00:00') > -1) {
+                let tempDay = new Date(jsonObject.list[i].dt_txt);
+                daysArray.push(dayOfWeek[tempDay.getDay()]);
+                iconsArray.push(jsonObject.list[i].weather[0].icon);
+                descIconArray.push(jsonObject.list[i].weather[0].description)
+                tempArray.push(jsonObject.list[i].main.temp);
+            }
         }
 
-        let finalResult = (t <= 50 && s > 3) ? `${f}°F` : 'N/A';
+        for (let i = 0; i < tempArray.length; i++) {
+            // --------------Creating HTML elements--------------------
+            let divCard = document.createElement('div');
+            let dayP = document.createElement('p');
+            let iconImg = document.createElement('img');
+            let tempP = document.createElement('p');
+            divCard.className = "forecast-day";
+            dayP.setAttribute('id', `dayOfWeek${i + 1}`);
+            iconImg.setAttribute('id', `icon${i + 1}`);
+            tempP.setAttribute('id', `temp${i + 1}`);
+            divCard.appendChild(dayP);
+            divCard.appendChild(iconImg);
+            divCard.appendChild(tempP);
+            document.getElementById("forecast").appendChild(divCard);
 
-        document.getElementById('windchill').textContent = finalResult;
-    })
-
-// Five days forecast
-const days = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
-let day1 = days[currentDate.getDay()];
-let day2 = days[currentDate.getDay() + 1];
-let day3 = days[currentDate.getDay() + 2];
-let day4 = days[currentDate.getDay() + 3];
-let day5 = days[currentDate.getDay() + 4];
-document.getElementById('day1').innerHTML = day1;
-document.getElementById('day2').innerHTML = day2;
-document.getElementById('day3').innerHTML = day3;
-document.getElementById('day4').innerHTML = day4;
-document.getElementById('day5').innerHTML = day5;
+            // ------------------Assigning values-----------------
+            document.getElementById(`dayOfWeek${i + 1}`).textContent = `${daysArray[i]}`;
+            document.getElementById(`icon${i + 1}`).setAttribute('src', `https://openweathermap.org/img/wn/${iconsArray[i]}@4x.png`);
+            document.getElementById(`icon${i + 1}`).setAttribute('alt', `${descIconArray[i]} icon`);
+            document.getElementById(`temp${i + 1}`).textContent = `${tempArray[i].toFixed(0)}°F`;
+        }
+    });
