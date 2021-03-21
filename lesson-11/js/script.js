9d7b10e2ee90f7a30dfc2c8527f21bf9e486ce13
gnzlplcs <palaciosgm@outlook.com>
2021-03-20 23:38:46 -0500

    events

--- lesson-11/js/script.js
+++ lesson-11/js/script.js
@@ -75,10 +75,10 @@ if ('IntersectionObserver' in window) {
 }
 
 
-// ---------------JSON Request-----------------------
-const requestURLtowns = "https://byui-cit230.github.io/weather/data/towndata.json";
+// ---------------Home JSON Request-----------------------
+const homeRequestURL = "https://byui-cit230.github.io/weather/data/towndata.json";
 
-fetch(requestURLtowns)
+fetch(homeRequestURL)
     .then(function (response) {
         return response.json();
     })
@@ -119,21 +119,23 @@ fetch(requestURLtowns)
     })
 
 // ---------------JSON Request-----------------------
-let cityID = 5604473;
+let prestonCityID = 5604473;
+let sodaSpringsCityID = 5607916;
+let fishHavenCityID = 5585010;
 let appid = "96903c0c4a665e1829d511ed75451893";
 
-// ----------Weather Summary-------------
-const requestURLweather = `https://api.openweathermap.org/data/2.5/weather?id=${cityID}&appid=${appid}&units=imperial`;
+// ----------Preston Weather Summary-------------
+const prestonRequestURLweather = `https://api.openweathermap.org/data/2.5/weather?id=${prestonCityID}&appid=${appid}&units=imperial`;
 
-fetch(requestURLweather)
+fetch(prestonRequestURLweather)
     .then((response) => response.json())
     .then((jsonObject) => {
         console.log(jsonObject);
-        document.getElementById('temperature').textContent = `${jsonObject.weather[0].main}, ${jsonObject.main.temp.toFixed(0)}`;
-        document.getElementById('max-temp').textContent = jsonObject.main.temp_max.toFixed(0);
-        document.getElementById('min-temp').textContent = jsonObject.main.temp_min.toFixed(0);
-        document.getElementById('humidity').textContent = jsonObject.main.humidity;
-        document.getElementById('wind').textContent = jsonObject.wind.speed;
+        document.getElementById('p-temperature').textContent = `${jsonObject.weather[0].main}, ${jsonObject.main.temp.toFixed(0)}`;
+        document.getElementById('p-max-temp').textContent = jsonObject.main.temp_max.toFixed(0);
+        document.getElementById('p-min-temp').textContent = jsonObject.main.temp_min.toFixed(0);
+        document.getElementById('p-humidity').textContent = jsonObject.main.humidity;
+        document.getElementById('p-wind').textContent = jsonObject.wind.speed;
 
         // -------------------Setting up Wind Chill----------------
         let t = parseFloat(jsonObject.main.temp);
@@ -145,13 +147,13 @@ fetch(requestURLweather)
             return result.toFixed(0);
         }
         let finalResult = (t <= 50 && s > 3) ? `${f}°F` : 'N/A';
-        document.getElementById('windchill').textContent = finalResult;
+        document.getElementById('p-windchill').textContent = finalResult;
     });
 
-// -------------Forecast------------
-const requestURLforecast = `https://api.openweathermap.org/data/2.5/forecast?id=${cityID}&appid=${appid}&units=imperial`;
+// -------------Preston Forecast------------
+const prestonRequestURLforecast = `https://api.openweathermap.org/data/2.5/forecast?id=${prestonCityID}&appid=${appid}&units=imperial`;
 
-fetch(requestURLforecast)
+fetch(prestonRequestURLforecast)
     .then((response) => response.json())
     .then((jsonObject) => {
         // ----------------Declaring variables---------------
@@ -179,18 +181,245 @@ fetch(requestURLforecast)
             let iconImg = document.createElement('img');
             let tempP = document.createElement('p');
             divCard.className = "forecast-day";
-            dayP.setAttribute('id', `dayOfWeek${i + 1}`);
-            iconImg.setAttribute('id', `icon${i + 1}`);
-            tempP.setAttribute('id', `temp${i + 1}`);
+            dayP.setAttribute('id', `p-dayOfWeek${i + 1}`);
+            iconImg.setAttribute('id', `p-icon${i + 1}`);
+            tempP.setAttribute('id', `p-temp${i + 1}`);
             divCard.appendChild(dayP);
             divCard.appendChild(iconImg);
             divCard.appendChild(tempP);
-            document.getElementById("forecast").appendChild(divCard);
+            document.getElementById("p-forecast").appendChild(divCard);
 
             // ------------------Assigning values-----------------
-            document.getElementById(`dayOfWeek${i + 1}`).textContent = `${daysArray[i]}`;
-            document.getElementById(`icon${i + 1}`).setAttribute('src', `https://openweathermap.org/img/wn/${iconsArray[i]}@4x.png`);
-            document.getElementById(`icon${i + 1}`).setAttribute('alt', `${descIconArray[i]} icon`);
-            document.getElementById(`temp${i + 1}`).textContent = `${tempArray[i].toFixed(0)}°F`;
+            document.getElementById(`p-dayOfWeek${i + 1}`).textContent = `${daysArray[i]}`;
+            document.getElementById(`p-icon${i + 1}`).setAttribute('src', `https://openweathermap.org/img/wn/${iconsArray[i]}@4x.png`);
+            document.getElementById(`p-icon${i + 1}`).setAttribute('alt', `${descIconArray[i]} icon`);
+            document.getElementById(`p-temp${i + 1}`).textContent = `${tempArray[i].toFixed(0)}°F`;
         }
     });
+
+// ----------Soda Springs Weather Summary-------------
+const sodaSpringsRequestURLweather = `https://api.openweathermap.org/data/2.5/weather?id=${sodaSpringsCityID}&appid=${appid}&units=imperial`;
+
+fetch(sodaSpringsRequestURLweather)
+    .then((response) => response.json())
+    .then((jsonObject) => {
+        console.log(jsonObject);
+        document.getElementById('ss-temperature').textContent = `${jsonObject.weather[0].main}, ${jsonObject.main.temp.toFixed(0)}`;
+        document.getElementById('ss-max-temp').textContent = jsonObject.main.temp_max.toFixed(0);
+        document.getElementById('ss-min-temp').textContent = jsonObject.main.temp_min.toFixed(0);
+        document.getElementById('ss-humidity').textContent = jsonObject.main.humidity;
+        document.getElementById('ss-wind').textContent = jsonObject.wind.speed;
+
+        // -------------------Setting up Wind Chill----------------
+        let t = parseFloat(jsonObject.main.temp);
+        let s = parseFloat(jsonObject.wind.speed);
+        let f = calc(t, s);
+
+        function calc(x, y) {
+            let result = 35.74 + (0.6215 * x) - (35.75 * Math.pow(y, 0.16)) + (0.4275 * x * Math.pow(y, 0.16));
+            return result.toFixed(0);
+        }
+        let finalResult = (t <= 50 && s > 3) ? `${f}°F` : 'N/A';
+        document.getElementById('ss-windchill').textContent = finalResult;
+    });
+
+// -------------Soda Springs Forecast------------
+const sodaSpringsRequestURLforecast = `https://api.openweathermap.org/data/2.5/forecast?id=${sodaSpringsCityID}&appid=${appid}&units=imperial`;
+
+fetch(sodaSpringsRequestURLforecast)
+    .then((response) => response.json())
+    .then((jsonObject) => {
+        // ----------------Declaring variables---------------
+        const dayOfWeek = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+        let daysArray = [];
+        let iconsArray = [];
+        let descIconArray = [];
+        let tempArray = [];
+
+        // --------------Setting up arrays----------------------
+        for (let i = 0; i < jsonObject.list.length; i++) {
+            if (jsonObject.list[i].dt_txt.indexOf('18:00:00') > -1) {
+                let tempDay = new Date(jsonObject.list[i].dt_txt);
+                daysArray.push(dayOfWeek[tempDay.getDay()]);
+                iconsArray.push(jsonObject.list[i].weather[0].icon);
+                descIconArray.push(jsonObject.list[i].weather[0].description)
+                tempArray.push(jsonObject.list[i].main.temp);
+            }
+        }
+
+        for (let i = 0; i < tempArray.length; i++) {
+            // --------------Creating HTML elements--------------------
+            let divCard = document.createElement('div');
+            let dayP = document.createElement('p');
+            let iconImg = document.createElement('img');
+            let tempP = document.createElement('p');
+            divCard.className = "forecast-day";
+            dayP.setAttribute('id', `ss-dayOfWeek${i + 1}`);
+            iconImg.setAttribute('id', `ss-icon${i + 1}`);
+            tempP.setAttribute('id', `ss-temp${i + 1}`);
+            divCard.appendChild(dayP);
+            divCard.appendChild(iconImg);
+            divCard.appendChild(tempP);
+            document.getElementById("ss-forecast").appendChild(divCard);
+
+            // ------------------Assigning values-----------------
+            document.getElementById(`ss-dayOfWeek${i + 1}`).textContent = `${daysArray[i]}`;
+            document.getElementById(`ss-icon${i + 1}`).setAttribute('src', `https://openweathermap.org/img/wn/${iconsArray[i]}@4x.png`);
+            document.getElementById(`ss-icon${i + 1}`).setAttribute('alt', `${descIconArray[i]} icon`);
+            document.getElementById(`ss-temp${i + 1}`).textContent = `${tempArray[i].toFixed(0)}°F`;
+        }
+    });
+
+// ----------Fish Haven Weather Summary-------------
+const fishHavenRequestURLweather = `https://api.openweathermap.org/data/2.5/weather?id=${fishHavenCityID}&appid=${appid}&units=imperial`;
+
+fetch(fishHavenRequestURLweather)
+    .then((response) => response.json())
+    .then((jsonObject) => {
+        console.log(jsonObject);
+        document.getElementById('fh-temperature').textContent = `${jsonObject.weather[0].main}, ${jsonObject.main.temp.toFixed(0)}`;
+        document.getElementById('fh-max-temp').textContent = jsonObject.main.temp_max.toFixed(0);
+        document.getElementById('fh-min-temp').textContent = jsonObject.main.temp_min.toFixed(0);
+        document.getElementById('fh-humidity').textContent = jsonObject.main.humidity;
+        document.getElementById('fh-wind').textContent = jsonObject.wind.speed;
+
+        // -------------------Setting up Wind Chill----------------
+        let t = parseFloat(jsonObject.main.temp);
+        let s = parseFloat(jsonObject.wind.speed);
+        let f = calc(t, s);
+
+        function calc(x, y) {
+            let result = 35.74 + (0.6215 * x) - (35.75 * Math.pow(y, 0.16)) + (0.4275 * x * Math.pow(y, 0.16));
+            return result.toFixed(0);
+        }
+        let finalResult = (t <= 50 && s > 3) ? `${f}°F` : 'N/A';
+        document.getElementById('fh-windchill').textContent = finalResult;
+    });
+
+// -------------Soda Springs Forecast------------
+const fishHavenRequestURLforecast = `https://api.openweathermap.org/data/2.5/forecast?id=${fishHavenCityID}&appid=${appid}&units=imperial`;
+
+fetch(fishHavenRequestURLforecast)
+    .then((response) => response.json())
+    .then((jsonObject) => {
+        // ----------------Declaring variables---------------
+        const dayOfWeek = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+        let daysArray = [];
+        let iconsArray = [];
+        let descIconArray = [];
+        let tempArray = [];
+
+        // --------------Setting up arrays----------------------
+        for (let i = 0; i < jsonObject.list.length; i++) {
+            if (jsonObject.list[i].dt_txt.indexOf('18:00:00') > -1) {
+                let tempDay = new Date(jsonObject.list[i].dt_txt);
+                daysArray.push(dayOfWeek[tempDay.getDay()]);
+                iconsArray.push(jsonObject.list[i].weather[0].icon);
+                descIconArray.push(jsonObject.list[i].weather[0].description)
+                tempArray.push(jsonObject.list[i].main.temp);
+            }
+        }
+
+        for (let i = 0; i < tempArray.length; i++) {
+            // --------------Creating HTML elements--------------------
+            let divCard = document.createElement('div');
+            let dayP = document.createElement('p');
+            let iconImg = document.createElement('img');
+            let tempP = document.createElement('p');
+            divCard.className = "forecast-day";
+            dayP.setAttribute('id', `fh-dayOfWeek${i + 1}`);
+            iconImg.setAttribute('id', `fh-icon${i + 1}`);
+            tempP.setAttribute('id', `fh-temp${i + 1}`);
+            divCard.appendChild(dayP);
+            divCard.appendChild(iconImg);
+            divCard.appendChild(tempP);
+            document.getElementById("fh-forecast").appendChild(divCard);
+
+            // ------------------Assigning values-----------------
+            document.getElementById(`fh-dayOfWeek${i + 1}`).textContent = `${daysArray[i]}`;
+            document.getElementById(`fh-icon${i + 1}`).setAttribute('src', `https://openweathermap.org/img/wn/${iconsArray[i]}@4x.png`);
+            document.getElementById(`fh-icon${i + 1}`).setAttribute('alt', `${descIconArray[i]} icon`);
+            document.getElementById(`fh-temp${i + 1}`).textContent = `${tempArray[i].toFixed(0)}°F`;
+        }
+    });
+
+// ---------------Preston Events JSON Request-----------------------
+const pEventsRequestURL = "https://byui-cit230.github.io/weather/data/towndata.json";
+
+fetch(pEventsRequestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        const towns = jsonObject['towns'];
+        for (let i = 0; i < towns.length; i++) {
+            if (towns[i].name == 'Preston') {
+                let card = document.createElement('section');
+                let divText = document.createElement('div');
+                let h2 = document.createElement('h2');
+                h2.textContent = "Upcoming Events";
+                divText.appendChild(h2);
+                for (let j = 0; j < towns[i].events.length; j++) {
+                    let events = document.createElement('p');
+                    events.textContent = towns[i].events[j];
+                    divText.appendChild(events);
+                }
+                card.appendChild(divText);
+                document.getElementById('preston-events').appendChild(card);
+            }
+        }
+    })
+
+    // ---------------Soda Springs Events JSON Request-----------------------
+const ssEventsRequestURL = "https://byui-cit230.github.io/weather/data/towndata.json";
+
+fetch(ssEventsRequestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        const towns = jsonObject['towns'];
+        for (let i = 0; i < towns.length; i++) {
+            if (towns[i].name == 'Soda Springs') {
+                let card = document.createElement('section');
+                let divText = document.createElement('div');
+                let h2 = document.createElement('h2');
+                h2.textContent = "Upcoming Events";
+                divText.appendChild(h2);
+                for (let j = 0; j < towns[i].events.length; j++) {
+                    let events = document.createElement('p');
+                    events.textContent = towns[i].events[j];
+                    divText.appendChild(events);
+                }
+                card.appendChild(divText);
+                document.getElementById('sodaSprings-events').appendChild(card);
+            }
+        }
+    })
+
+    // ---------------Preston Events JSON Request-----------------------
+const fhEventsRequestURL = "https://byui-cit230.github.io/weather/data/towndata.json";
+
+fetch(fhEventsRequestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        const towns = jsonObject['towns'];
+        for (let i = 0; i < towns.length; i++) {
+            if (towns[i].name == 'Fish Haven') {
+                let card = document.createElement('section');
+                let divText = document.createElement('div');
+                let h2 = document.createElement('h2');
+                h2.textContent = "Upcoming Events";
+                divText.appendChild(h2);
+                for (let j = 0; j < towns[i].events.length; j++) {
+                    let events = document.createElement('p');
+                    events.textContent = towns[i].events[j];
+                    divText.appendChild(events);
+                }
+                card.appendChild(divText);
+                document.getElementById('fishHaven-events').appendChild(card);
+            }
+        }
+    })
